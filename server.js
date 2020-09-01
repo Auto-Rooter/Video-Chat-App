3d9261d007c51c6c39fc3a767cf9bd51fd2f2442
Autorooter <hadi.assalem@gmail.com>
2020-09-01 08:43:25 +0200

    Server done

--- server.js
+++ server.js
@@ -5,6 +5,33 @@ const server = http.createServer(app);
 const socket = require("socket.io");
 const io = socket(server);
 
+const rooms = {};
 
+io.on("connection", socket => {
+    socket.on("join room", roomID => {
+        if(rooms[roomID]){
+            rooms[roomID].push(socket.id);
+        } else {
+            rooms[roomID] = [socket.id];
+        }
+        const otherUser = rooms[roomID].find(id => id !== socket.io);
+        if(otherUser){
+            socket.emit("other user", otherUser);
+            socket.to(otherUser).emit("user joined", socket.id);
+        }
+    });
+
+    socket.on("offer", payload => {
+        io.to(payload.target).emit("offer", payload);
+    });
+
+    socket.on("answer", payload => {
+        io.to(payload.target).emit("answer", payload);
+    });
+
+    socket.on("ice-candidate", incoming => {
+        io.to(payload.target).emit("ice-candidate", payload);
+    });
+});
 
 server.listen(8000, () => console.log("Server is running on port 8000"));
